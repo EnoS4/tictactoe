4fd09a1ca4470cd4e7131ac68d118ba415e5536f
EnoS4 <saya.loveup.99@gmail.com>
2016-04-03 15:31:33 +0900

    Create BoderStatus.cs

---
+++ BoderStatus.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.text;
+using System.Threading.Tasks;
+
+namespace tictactoe{
+  public enum BODER_STATUS{
+    NONE,
+    
+    HIGHT_FRONT,
+    HIGHT_MIDDLE,
+    HIGHT_BACK,
+    
+    WIDHT_FRONT,
+    WIDHT_MIDDLE,
+    WIDHT_BACK,
+    
+    SLASH_FRONT,
+    SLASH_BACK,
+    }
+  
+  class BoderStatus {
+  }
+}
